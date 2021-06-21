8819d9e80675c91377ac10c6a2bddbab8c66bcd5
Hriday Keswani <hridaykeswani@gmail.com>
2021-06-21 19:41:21 +0530

    added onclick to each button

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click00(View view) {
+    }
+
+    public void click01(View view) {
+    }
+
+    public void click02(View view) {
+    }
+
+    public void click10(View view) {
+    }
+
+    public void click11(View view) {
+    }
+
+    public void click12(View view) {
+    }
+
+    public void click20(View view) {
+    }
+
+    public void click21(View view) {
+    }
+
+    public void click22(View view) {
     }
 }
